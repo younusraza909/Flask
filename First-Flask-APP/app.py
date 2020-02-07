2e07df9ee59b185c6d84dfdaed1950b91846b4b4
younusraza909 <younusraza909@gmail.com>
2020-02-07 11:03:27 +0500

    created a blog dictionary and render data from it

--- First-Flask-APP/app.py
+++ First-Flask-APP/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
+blog={
+    "name":"My First Blog",
+    "posts":{
+        1:{
+            "post_id":1,
+            "title":"First Post",
+            "content":"First Post Content"
+        }
+    }
+}
+
+
 
 @app.route("/")
 def home():
-    return "Hello,world"
+    return render_template("home.html")
 
+@app.route("/post<int:post_id>")
+def post(post_id):
+    post=blog["posts"][post_id]
+    return post["title"]
 
 app.run(debug=True)    
