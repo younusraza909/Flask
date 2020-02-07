121470f25898e70f93f585b4b541b834bd110a61
younusraza909 <younusraza909@gmail.com>
2020-02-07 12:10:01 +0500

    First-App Completed

--- First-Flask-APP/app.py
+++ First-Flask-APP/app.py
@@ -1,26 +1,31 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 
 app=Flask(__name__)
 blog={
     "name":"My First Blog",
-    "posts":{
-        1:{
-            "post_id":1,
-            "title":"First Post",
-            "content":"First Post Content"
-        }
-    }
+    "posts":{}
 }
 
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",blog=blog)
 
 @app.route("/post<int:post_id>")
 def post(post_id):
-    post=blog["posts"][post_id]
-    return post["title"]
+    post=blog["posts"].get(post_id)
+    if not post:
+        return render_template("404NotFound.html",message=f"The post having Id {post_id} Was Not found")
+    return render_template("post.html",post=post)
 
-app.run(debug=True)    
+@app.route("/post/create",methods=["GET","POST"])
+def create():
+    if request.method=="POST":
+        title=request.form.get("title")
+        content=request.form.get("content")
+        post_id=len(blog["posts"])
+        blog["posts"][post_id]={"post_id":post_id,"title":title,"content":content}
+        return redirect(url_for("post",post_id=post_id))
+    return render_template("create.html")
+app.run()    
