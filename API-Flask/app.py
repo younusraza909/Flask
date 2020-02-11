0f4c3d898b419255608332e062d2dccf67d39ab1
younusraza909 <younusraza909@gmail.com>
2020-02-11 14:48:20 +0500

    Have Completly implemented endpoints for our app

--- API-Flask/app.py
+++ API-Flask/app.py
@@ -2,7 +2,7 @@
 #GET:We Have To Send Some Data Back
 #In Browser Perspective its opposite
 
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 app=Flask(__name__)
 
 
@@ -22,11 +22,19 @@ stores=[
 
 @app.route("/store",methods=["POST"])
 def create_store():
-    pass
+    request_data=request.get_json()
+    new_stores={"name":request_data["name"],
+                "items":[]}
+    stores.append(new_stores)
+    return jsonify(new_stores)            
 
 @app.route("/store/<string:name>")
 def get_store():
-    pass
+    for i in stores:
+        if i["name"]==name:
+            return jsonify(i)
+        else:
+            return jsonify({"messgae":"Store Not Found"})
 
 @app.route("/store")
 def get_stores():
@@ -34,11 +42,23 @@ def get_stores():
 
 @app.route("/store/<string:name>/item",methods=["POST"])
 def create_item_in_store():
-    pass
-
+    request_data=request.get_json()
+    for store in stores:
+        if store["name"]=name:
+            new_item={
+                'name':request_data["name"],
+                "price":request_data["price"]
+            }
+            store["items"].append(new_item)
+            return jsonify(new_item)
+        else:
+            return jsonify({"message":"No Store With Given Name Available"})    
 @app.route("/store/<string:name>/item")
 def get_items_in_store():
-    pass
+    for store in stores:
+        if store["name"]==name:
+            return jsonify({"item":store["item"]})
+        return jsonify({"message":"Store Not Found"})    
 
 
 app.run()
