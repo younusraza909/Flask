833b3d00c548047cc7e0e7e588f71b13a84b377f
younusraza909 <younusraza909@gmail.com>
2020-02-12 15:33:07 +0500

    Restfull API Part2

--- RestFull-App/app.py
+++ RestFull-App/app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource,Api
 
 app=Flask(__name__)
 api=Api(app)
 
-class Student(Resource):
-    def get(self,name):
-        return {"student":name}
+items=[]
 
-api.add_resource(Student,'/student/<string:name>')
+class Item(Resource):
+    def get(self,name):
+        for i in items:
+            if i["name"] == name:
+                return i
+        return{"item":None},404
+    def post(self,name):
+        data=request.get_json()
+        item={"name":name,"price":data["price"]}
+        items.append(item) 
+        return item ,201 #hhtp code for created
+
+class ItemList(Resource):
+    def get(self):
+        return {"items":items}       
+
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,"/items")
 
 app.run(port=5000)
